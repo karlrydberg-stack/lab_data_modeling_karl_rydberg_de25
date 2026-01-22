7215fa5121d974f94f1a662b6d8f5c43acdf6904
Karl Rydberg <kalle.rydberg02@gmail.com>
2026-01-22 17:00:55 +0100

    Added models, some sql-scripts

--- sql/join_student_program.sql
+++ sql/join_student_program.sql
@@ -6,4 +6,5 @@ LEFT JOIN class c ON s.class_id = c.class_id
 LEFT JOIN courseclass cc ON c.class_id = cc.class_id
 LEFT JOIN course co ON cc.course_id = co.course_id
 LEFT JOIN curriculum cu ON cu.curriculum_id = co.curriculum_id
-LEFT JOIN program pr ON cu.program_id = pr.program_id;
+LEFT JOIN program pr ON cu.program_id = pr.program_id
+WHERE pr.program_id = 1;
